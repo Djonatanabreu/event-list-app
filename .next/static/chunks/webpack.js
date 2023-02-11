d02d6821ccb9ed6993c394ba44daee961a823007
Djonatan Abreu <djonatan@Djonatans-MacBook-Air.local>
2023-02-11 18:55:00 -0300

    fixing new nextjs files

--- .next/static/chunks/webpack.js
+++ .next/static/chunks/webpack.js
@@ -131,7 +131,7 @@
 /******/ 	
 /******/ 	/* webpack/runtime/getFullHash */
 /******/ 	!function() {
-/******/ 		__webpack_require__.h = function() { return "75b8b610a9c0cff6"; }
+/******/ 		__webpack_require__.h = function() { return "1f526b780c45610b"; }
 /******/ 	}();
 /******/ 	
 /******/ 	/* webpack/runtime/global */

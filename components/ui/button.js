0ab2366677424a89dee6811ea54f4d403dd25e80
Djonatan Abreu <djonatan@Djonatans-MacBook-Air.local>
2023-02-14 02:01:26 -0300

    feat: events menagment, filter"

--- components/ui/button.js
+++ components/ui/button.js
@@ -3,11 +3,15 @@ import Link from 'next/link';
 import classes from './button.module.css';
 
 export function Button(props) {
-  return (
-    <Link href={props.link} className={classes.btn}>
-      {props.children}
-    </Link>
-  )
+  if (props.link) {
+    return (
+      <Link href={props.link} className={classes.btn}>
+        {props.children}
+      </Link>
+    )
+  }
+
+  return <button className={classes.btn} onClick={props.onCLick} >{props.children}</button>
 };
 
 export default Button;
